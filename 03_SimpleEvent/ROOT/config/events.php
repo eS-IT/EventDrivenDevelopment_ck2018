cafeb20bf51cf65283a13b7e17b1c465d847ced2
Patrick Froch <patrick.froch@easySolutionsIT.de>
2018-06-08 14:47:56 +0200

    REMOVED: Überflüssige Kommentare

--- 03_SimpleEvent/ROOT/config/events.php
+++ 03_SimpleEvent/ROOT/config/events.php
@@ -1,6 +1,6 @@
 <?php
 # 03_SimpleEvent/ROOT/config/events.php
 $events['greeting.event'][1024] = ['Esit\Listener\OnGreetingListener', 'generateGreeting'];
-$events['greeting.event'][2048] = ['Esit\Listener\OnGreetingListenerTwo', 'modifyGreeting']; # new!
+$events['greeting.event'][512] = ['Esit\Listener\OnGreetingListenerTwo', 'modifyGreeting']; # new!
 
 return $events;
